12bcca27115b3c8e7a09c7bf7a221145499c8747
szympie <justpiech@gmail.com>
2016-04-08 13:43:37 +0200

    Code Refactored

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-#include <fstream>
-
 #include "BitMap.h"
-#include "BinaryArray.h"
 
 using namespace std;
 
 int main(){
-	const char *path ="/Users/apple/ClionProjects/NeuralNet/a.bmp";
+	const char *path = "/Users/apple/ClionProjects/NeuralNet/x.bmp";
 	BitMap newBmp = BitMap(path);
 	newBmp.BmpToBinaryArrayConversion();
-	cout << "eloeleo";
-	cout << sizeof(BITMAPFILEHEADER)<<endl;
-	cout << sizeof(BITMAPINFOHEADER)<<endl;
-
-
-
+	newBmp.PrintBitmapInBinaryArray();
 	return 0;
 }
